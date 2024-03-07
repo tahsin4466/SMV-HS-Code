33a8691ef199863750716a98e3bc13729e6e4d75
Thunderstroyer <109246944+Thunderstroyer@users.noreply.github.com>
2024-03-06 18:10:45 -0800

    Add files via upload

--- H_S_FET_Testing.ino
+++ H_S_FET_Testing.ino
@@ -1,12 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(4, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(26, OUTPUT);
+#define BOARD 0
+//0 = back left/front left
+//1 = back right
+//2 = front right
+//3 = front center
 
-  digitalWrite(4, HIGH); //D4
-  digitalWrite(1, HIGH); //RX
-  digitalWrite(26, HIGH); //A0
+#if BOARD == 0
+  #define MOSFET1 4
+  #define MOSFET2 26
+  #define MOSFET3 1
+#elif BOARD == 1
+  #define MOSFET1 9
+  #define MOSFET2 5
+  #define MOSFET3 6
+#elif BOARD == 2
+  #define MOSFET1 10
+  #define MOSFET2 9
+#elif BOARD == 3
+  #define MOSFET1 6
+#endif
+
+void setup() {
+  pinMode(MOSFET1, OUTPUT);
+  if (BOARD != 3) {
+    pinMode(MOSFET2, OUTPUT);
+  }
+  if (BOARD == 0 || BOARD == 1) {
+    pinMode(MOSFET3, OUTPUT);
+  }
+
+  digitalWrite(MOSFET1, HIGH);
+  if (BOARD != 3) {
+    digitalWrite(MOSFET2, HIGH);
+  }
+  if (BOARD == 0 || BOARD == 1) {
+    digitalWrite(MOSFET3, HIGH);
+  }
+
 }
 
 void loop() {
